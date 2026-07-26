367a098d543e1ceee327d7e8fdcb5671c2c61ae8
johnadamsmahama <johnadamsmahama@gmail.com>
2026-07-25 17:20:48 -0700

    Exclude app-build-manifest from service worker precaching
    Add buildExcludes to prevent caching of app-build-manifest.json.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development', // avoids SW caching hassles while developing
+  buildExcludes: [/app-build-manifest\.json$/],
   fallbacks: {
     document: '/offline',
   },
