9df65d13b4a4e86c429691cfac7b97a44f467a0d
johnadamsmahama <johnadamsmahama@gmail.com>
2026-08-02 17:44:22 -0700

    Add ProfileLayout component with AppShell wrapper

---
+++ app/profile/layout.tsx
@@ -0,0 +1,5 @@
+import AppShell from '@/components/dashboard/AppShell';
+
+export default function ProfileLayout({ children }: { children: React.ReactNode }) {
+  return <AppShell>{children}</AppShell>;
+}
