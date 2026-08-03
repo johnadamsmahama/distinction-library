f6f33ea0fe9010c7171efc37e321ad1c9e8cbd26
johnadamsmahama <johnadamsmahama@gmail.com>
2026-08-02 17:37:22 -0700

    Create SuccessCentreLayout component with AppShell

---
+++ app/success-centre/layout.tsx
@@ -0,0 +1,5 @@
+import AppShell from '@/components/dashboard/AppShell';
+
+export default function SuccessCentreLayout({ children }: { children: React.ReactNode }) {
+  return <AppShell>{children}</AppShell>;
+}
