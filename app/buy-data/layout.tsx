3d0f7569f5510c423eee182fed6d367a608a8c17
johnadamsmahama <johnadamsmahama@gmail.com>
2026-08-03 18:57:06 -0700

    Create layout.tsx

---
+++ app/buy-data/layout.tsx
@@ -0,0 +1,9 @@
+// app/buy-data/layout.tsx
+
+export default function BuyDataLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="px-6 py-16">{children}</div>;
+}
