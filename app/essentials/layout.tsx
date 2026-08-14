dc60922df4a577f78ba2a8811afa603945c79aa6
johnadamsmahama <johnadamsmahama@gmail.com>
2026-08-14 02:52:23 +0000

    Add EssentialsLayout component with AppShell

---
+++ app/essentials/layout.tsx
@@ -0,0 +1,4 @@
+import AppShell from '@/components/dashboard/AppShell';
+export default function EssentialsLayout({ children }: { children: React.ReactNode }) {
+  return <AppShell>{children}</AppShell>;
+}
