b218103738a9b52af51091e9643db61a90044d2e
johnadamsmahama <johnadamsmahama@gmail.com>
2026-08-28 17:31:51 +0000

    Add dynamic export to Stats component

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,6 +1,7 @@
 import { createClient } from '@/lib/supabase/server';
 
 export const revalidate = 0;
+export const dynamic = 'force-dynamic';
 
 export default async function Stats() {
   const supabase = createClient();
