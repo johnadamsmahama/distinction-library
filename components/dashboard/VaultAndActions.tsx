cb7542770a1b844e6de5717201febf6884fa636c
johnadamsmahama <johnadamsmahama@gmail.com>
2026-09-05 11:29:08 +0000

    Change href from '/papers' to '/library'

--- components/dashboard/VaultAndActions.tsx
+++ components/dashboard/VaultAndActions.tsx
@@ -78,7 +78,7 @@ const ACTIONS: Action[] = [
     grain: true,
   },
   {
-    href: '/papers',
+    href: '/library',
     label: 'Library',
     desc: 'Browse and download past papers & materials',
     path: 'M4 19.5A2.5 2.5 0 016.5 17H20M6.5 2H20v20H6.5A2.5 2.5 0 014 19.5v-15A2.5 2.5 0 016.5 2z',
