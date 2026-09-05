d721772f34b35c2dd93e2f2493960c451f362254
johnadamsmahama <johnadamsmahama@gmail.com>
2026-09-05 11:19:55 +0000

    Create LibraryLayout component using AppShell

---
+++ app/library/layout.tsx
@@ -0,0 +1,5 @@
+import AppShell from '@/components/dashboard/AppShell';
+
+export default function LibraryLayout({ children }: { children: React.ReactNode }) {
+  return <AppShell>{children}</AppShell>;
+}
