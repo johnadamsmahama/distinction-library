11750c7376b96ad39ccfb87460627ea06f187e7a
johnadamsmahama <johnadamsmahama@gmail.com>
2026-09-09 00:40:30 +0000

    Adjust styling and layout in Stats component

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -28,7 +28,7 @@ export default async function Stats() {
   ];
 
   return (
-    <section className="relative bg-navy py-20 px-7 overflow-hidden">
+    <section className="relative bg-navy py-24 px-7 overflow-hidden">
       <div
         className="absolute inset-0 pointer-events-none"
         style={{
@@ -37,22 +37,22 @@ export default async function Stats() {
           backgroundSize: '44px 44px',
         }}
       />
-      <div className="relative z-10 max-w-content mx-auto flex flex-wrap justify-center gap-[28px]">
+      <div className="relative z-10 max-w-content mx-auto flex flex-wrap justify-center gap-[22px]">
         {STATS.map((s, i) => (
           <div
             key={s.label}
-            className="relative w-[148px] h-[153px] border border-gold/50 flex flex-col items-center justify-center text-center pt-1 before:content-[''] before:absolute before:inset-[8px] before:border before:border-gold/30"
-            style={{ transform: `rotate(${[-3, 2, -2, 4][i % 4]}deg)`, marginTop: [0, 10, 0, 8][i % 4] }}
+            className="relative w-[118px] h-[122px] border border-gold/50 flex flex-col items-center justify-center text-center pt-1 before:content-[''] before:absolute before:inset-[6px] before:border before:border-gold/30"
+            style={{ transform: `rotate(${[-3, 2, -2, 4][i % 4]}deg)`, marginTop: [0, 8, 0, 6][i % 4] }}
           >
             <svg
               viewBox="0 0 24 24"
-              className="relative z-10 w-[23px] h-[23px] mb-2 stroke-gold fill-none"
+              className="relative z-10 w-[18px] h-[18px] mb-1.5 stroke-gold fill-none"
               strokeWidth={2.2}
             >
               <path d={s.path} />
             </svg>
-            <div className="relative z-10 font-display font-black text-[26px] text-gold-light">{s.num}</div>
-            <div className="relative z-10 font-condensed font-extrabold text-[9.5px] uppercase tracking-[.07em] text-white/70 mt-1.5">
+            <div className="relative z-10 font-display font-black text-[21px] text-gold-light">{s.num}</div>
+            <div className="relative z-10 font-condensed font-extrabold text-[7.5px] uppercase tracking-[.07em] text-white/70 mt-1">
               {s.label}
             </div>
           </div>
